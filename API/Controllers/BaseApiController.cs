b9d5ccb1a31bcd9a5e30a750c09b786e4630b08c
Donna Cruz <130470174+donnac94@users.noreply.github.com>
2023-12-05 05:14:58 +0000

    End of section 4.

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,5 +1,6 @@
 
 
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -9,6 +10,12 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator 
+        ??= HttpContext.RequestServices.GetService<IMediator>();
+
+
+
     }
 }
