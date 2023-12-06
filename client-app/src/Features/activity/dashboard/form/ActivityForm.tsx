1347716c64c46f4ff43658ec8ecbb6e22c37527d
Donna Cruz <130470174+donnac94@users.noreply.github.com>
2023-12-06 08:40:37 +0000

    adding uuid id for when an activity is created

--- client-app/src/Features/activity/dashboard/form/ActivityForm.tsx
+++ client-app/src/Features/activity/dashboard/form/ActivityForm.tsx
@@ -36,7 +36,7 @@ export default function ActivityForm({
     setActivity({ ...activity, [name]: value });
   }
 
-  //To do: Figure out why i cant edit the Description, category, date
+
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit} autoComplete="Off">
@@ -49,19 +49,19 @@ export default function ActivityForm({
         <Form.TextArea
           placeholder="Description"
           value={activity.description}
-          name="Description"
+          name="description"
           onChange={handleInputOnChange}
         />
         <Form.Input
           placeholder="Category"
           value={activity.category}
-          name="Category"
+          name="category"
           onChange={handleInputOnChange}
         />
         <Form.Input
           placeholder="Date"
           value={activity.date}
-          name="Date"
+          name="date"
           onChange={handleInputOnChange}
         />
         <Form.Input
