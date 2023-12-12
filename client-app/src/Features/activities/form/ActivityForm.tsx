889ac3db25daedced46201102e44a7c71aa58f78
Donna Cruz <130470174+donnac94@users.noreply.github.com>
2023-12-12 00:04:29 +0000

    end of section 9.
    - Styling activity list
    - grouping activities by date
    - styling list items
    - activity details page
    - populating the detailed components
    - add activity filter component
    - styling homepage

--- client-app/src/Features/activities/form/ActivityForm.tsx
+++ client-app/src/Features/activities/form/ActivityForm.tsx
@@ -1,11 +1,11 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
-import { useStore } from "../../../../app/stores/store";
+import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../../app/models/activity";
-import LoadingComponent from "../../../../app/layout/LoadingComponent";
-import { v4 as uuid } from 'uuid';
+import { Activity } from "../../../app/models/activity";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { v4 as uuid } from "uuid";
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
@@ -36,10 +36,14 @@ export default observer(function ActivityForm() {
   function handleSubmit() {
     if (!activity.id) {
       activity.id = uuid();
-      createActivity(activity).then(() => navigate (`/activities/${activity.id}`))
+      createActivity(activity).then(() =>
+        navigate(`/activities/${activity.id}`)
+      );
     } else {
-      updateActivity(activity).then(() => navigate(`/activities/${activity.id}`)); 
-    } 
+      updateActivity(activity).then(() =>
+        navigate(`/activities/${activity.id}`)
+      );
+    }
   }
 
   function handleInputOnChange(
@@ -96,7 +100,13 @@ export default observer(function ActivityForm() {
           type="submit"
           content="Submit"
         />
-        <Button  as = {Link} to='/activities' floated="right" type="submit" content="Cancel" />
+        <Button
+          as={Link}
+          to="/activities"
+          floated="right"
+          type="submit"
+          content="Cancel"
+        />
       </Form>
     </Segment>
   );
